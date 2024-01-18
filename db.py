487de73b633fe36e686c1a1a5a1bb019dd403e72
Senya <115795609+senyaghtbs@users.noreply.github.com>
2024-01-18 15:55:08 +0300

    123213
    31231

---
+++ db.py
@@ -0,0 +1,56 @@
+import sqlite3
+
+# создаем бд
+conn = sqlite3.connect('feedback.db', check_same_thread=False)
+cursor = conn.cursor()
+
+# таблица отзывов
+cursor.execute('''CREATE TABLE IF NOT EXISTS feedbacks
+               (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, feedback TEXT)''')
+conn.commit()
+
+#провелка на наличия таблицы
+cursor.execute("PRAGMA table_info(feedbacks)")
+columns = [info[1] for info in cursor.fetchall()]  # Получаем список названий колонок
+if 'created_at' not in columns:
+    cursor.execute('ALTER TABLE feedbacks ADD COLUMN created_at DATE')
+    conn.commit()
+
+# Закрываем соединение с БД
+cursor.close()
+conn.close()
+
+def init_db():
+    conn = sqlite3.connect('feedback.db', check_same_thread=False)
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS feedbacks
+                   (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, feedback TEXT)''')
+    conn.commit()
+    cursor.execute("PRAGMA table_info(feedbacks)")
+    columns = [info[1] for info in cursor.fetchall()]
+    if 'created_at' not in columns:
+        cursor.execute('ALTER TABLE feedbacks ADD COLUMN created_at DATE')
+        conn.commit()
+    cursor.close()
+    conn.close()
+
+def get_feedbacks():
+    with sqlite3.connect('feedback.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM feedbacks')
+        feedbacks = cursor.fetchall()
+    return feedbacks
+
+# в database.py
+def save_feedback(user_id, feedback, created_at):
+    with sqlite3.connect('feedback.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO feedbacks (user_id, feedback, created_at) VALUES (?, ?, ?)', (user_id, feedback, created_at))
+        conn.commit()
+
+def get_feedbacks_by_date(date):
+    with sqlite3.connect('feedback.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM feedbacks WHERE created_at = ?', (date,))
+        feedbacks = cursor.fetchall()
+    return feedbacks
--- main.py
+++ main.py
@@ -1,27 +1,10 @@
-import sqlite3
 import telebot
 from telebot import types
 import datetime
+from db import init_db, get_feedbacks, save_feedback, get_feedbacks_by_date
 
-# создаем бд
-conn = sqlite3.connect('feedback.db', check_same_thread=False)
-cursor = conn.cursor()
-
-# таблица отзывов
-cursor.execute('''CREATE TABLE IF NOT EXISTS feedbacks
-               (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, feedback TEXT)''')
-conn.commit()
-
-#провелка на наличия таблицы
-cursor.execute("PRAGMA table_info(feedbacks)")
-columns = [info[1] for info in cursor.fetchall()]  # Получаем список названий колонок
-if 'created_at' not in columns:
-    cursor.execute('ALTER TABLE feedbacks ADD COLUMN created_at DATE')
-    conn.commit()
-
-# Закрываем соединение с БД
-cursor.close()
-conn.close()
+# проверка наличия бд
+init_db()
 
 token = '6408738573:AAGsSAosjsqsXvlM_6JIzKKDP-LLaekGi2E'
 bot = telebot.TeleBot(token)
@@ -29,35 +12,23 @@ bot = telebot.TeleBot(token)
 ADMIN_USER_ID = 423672005
 
 @bot.message_handler(commands=['get_feedback'])
-def get_feedback(message):
+def handle_get_feedback(message):
     if message.from_user.id == ADMIN_USER_ID:
-        with sqlite3.connect('feedback.db') as conn:
-            cursor = conn.cursor()
-            cursor.execute('SELECT * FROM feedbacks')
-            feedbacks = cursor.fetchall()
-            response = "Список всех отзывов:\n\n"
-            for feedback in feedbacks:
-                response += f"ID: {feedback[0]}\nUser ID: {feedback[1]}\nFeedback: {feedback[2]}\n\n"
-            bot.send_message(message.chat.id, response)
+        feedbacks = get_feedbacks()
+        response = "Список всех отзывов:\n\n"
+        for feedback in feedbacks:
+            response += f"ID: {feedback[0]}\nUser ID: {feedback[1]}\nFeedback: {feedback[2]}\n\n"
+        bot.send_message(message.chat.id, response)
     else:
         bot.send_message(message.chat.id, 'У вас нет доступа к этой команде.')
 
-
-def save_feedback(message):
-    feedback = message.text
+def process_save_feedback(message):
+    feedback_text = message.text
     user_id = message.from_user.id
-    created_at = datetime.datetime.now().strftime('%Y-%m-%d') # Форматируем текущую дату
-    # записываем отзыв в бд
-    with sqlite3.connect('feedback.db') as conn:
-        cursor = conn.cursor()
-        # Обратите внимание на добавленный столбец created_at
-        cursor.execute('INSERT INTO feedbacks (user_id, feedback, created_at) VALUES (?, ?, ?)', (user_id, feedback, created_at))
-        conn.commit()
+    created_at = datetime.datetime.now().strftime('%Y-%m-%d')
     bot.send_message(message.chat.id, 'Спасибо за ваш отзыв!', reply_markup=types.ReplyKeyboardRemove())
-
-
 @bot.message_handler(commands=['get_feedback_by_date'])
-def get_feedback_by_date(message):
+def handle_get_feedback_by_date(message):
     if message.from_user.id == ADMIN_USER_ID:
         msg = bot.send_message(message.chat.id, 'Введите дату для получения отзывов в формате ГГГГ-ММ-ДД:')
         bot.register_next_step_handler(msg, show_feedbacks_by_date)
@@ -67,23 +38,19 @@ def get_feedback_by_date(message):
 def show_feedbacks_by_date(message):
     date = message.text
     try:
-        # Проверка правильности формата даты
-        datetime.datetime.strptime(date, '%Y-%m-%d')
+        datetime.datetime.strptime(date, '%Y-%m-%d')  # проверка даты
     except ValueError:
         bot.send_message(message.chat.id, 'Неправильный формат даты. Используйте ГГГГ-ММ-ДД.')
         return
 
-    with sqlite3.connect('feedback.db') as conn:
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM feedbacks WHERE created_at = ?', (date,))
-        feedbacks = cursor.fetchall()
-        if feedbacks:
-            response = "Отзывы за дату {}:\n\n".format(date)
-            for feedback in feedbacks:
-                response += f"ID: {feedback[0]}\nUser ID: {feedback[1]}\nFeedback: {feedback[2]}\nДата: {feedback[3]}\n\n"
-        else:
-            response = "Отзывы за дату {} не найдены.".format(date)
-        bot.send_message(message.chat.id, response)
+    feedbacks = get_feedbacks_by_date(date)
+    if feedbacks:
+        response = f"Отзывы за дату {date}:\n\n"
+        for feedback in feedbacks:
+            response += f"ID: {feedback[0]}\nUser ID: {feedback[1]}\nFeedback: {feedback[2]}\nДата: {feedback[3]}\n\n"
+    else:
+        response = f"Отзывы за дату {date} не найдены."
+    bot.send_message(message.chat.id, response)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -108,7 +75,5 @@ def process_feedback_step(message):
         bot.register_next_step_handler(msg, save_feedback)
     else:
         bot.send_message(message.chat.id, 'Отмена действия.', reply_markup=types.ReplyKeyboardRemove())
-
-
 
 bot.infinity_polling()
